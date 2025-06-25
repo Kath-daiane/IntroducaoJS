f7c3beb7ae06236968dd01e8ebc910bd62484394
Kath-daiane <kath.daiane.p@gmail.com>
2025-06-25 08:59:44 -0300

    adiciona página HTML de calculadora com funcionalidade de soma

---
+++ scripts/calculadora.js
@@ -0,0 +1,8 @@
+function somar () {
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
+    let soma = parseFloat(num1) + parseFloat(num2);
+    document.getElementById("resultado").innerHTML = "Resultado: " + soma;
+   
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
