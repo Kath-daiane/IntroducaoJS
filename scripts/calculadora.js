085e021c47c29e5a79c60055fef8d73a202def46
Kath-daiane <kath.daiane.p@gmail.com>
2025-06-25 09:36:23 -0300

    adiciona funções de subtração, multiplicação e divisão à calculadora

--- scripts/calculadora.js
+++ scripts/calculadora.js
@@ -3,5 +3,31 @@ function somar () {
     let num2 = document.getElementById("num2").value;
     let soma = parseFloat(num1) + parseFloat(num2);
     document.getElementById("resultado").innerHTML = "Resultado: " + soma;
+
+}
+
+function subtrair() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let subtracao = num1 - num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + subtracao;
+}
+
+function multiplicar() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let multiplicacao = num1 * num2;
+    document.getElementById("resultado").innerHTML = "Resultado: " + multiplicacao;
+}
+
+function dividir() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
     
+    if (num2 === 0) {
+        document.getElementById("resultado").innerHTML = "Erro: divisão por zero!";
+    } else {
+        let divisao = num1 / num2;
+        document.getElementById("resultado").innerHTML = "Resultado: " + divisao;
+    }
 }
